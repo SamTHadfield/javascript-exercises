bc0d19cff5249968e2bf1c466d55e6192e3c7dfe
Sam Hadfield <sthtech@protonmail.com>
2024-10-17 10:57:46 -0500

    Solve coding challenge

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    result += string;
+  }
+  return result;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// //const repeatString = function() {
+
+// };
