b86384df19ba394a43aac029c75e5e4e24360fba
Sam Hadfield <sthtech@protonmail.com>
2024-10-28 11:46:44 -0500

    Write function that passes all reverseString tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let newArr = str.split("");
+  newArr.reverse();
+  newStr = newArr.join("");
+  return newStr;
 };
 
+reverseString("");
 // Do not edit below this line
 module.exports = reverseString;
