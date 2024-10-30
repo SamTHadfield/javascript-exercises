ea9e39299574fda2f48c7d099a5ef0d2347b9d87
Sam Hadfield <sthtech@protonmail.com>
2024-10-30 16:03:29 -0500

    Write function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  let sum = 0;
 
+  if (startNum < 0 || endNum < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+    return "ERROR";
+  } else if (startNum < endNum) {
+    for (let i = startNum; i <= endNum; i++) {
+      sum += i;
+    }
+  } else if (startNum > endNum) {
+    for (let i = startNum; i >= endNum; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
+sumAll(100, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
