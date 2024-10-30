830f5d9dbd0c95e44f85a934700a95845ab15cc0
Sam Hadfield <sthtech@protonmail.com>
2024-10-30 17:13:42 -0500

    Write function to pass all leap year tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) return true;
+  if (year % 4 === 0 && year % 100 === 0) return false;
+  if (year % 100 === 0 && year % 400 === 0) return true;
+  if (year % 4 === 0) return true;
+  if (year % 100 === 0) return false;
+  return false;
 };
+
+leapYears(1997);
 
 // Do not edit below this line
 module.exports = leapYears;
