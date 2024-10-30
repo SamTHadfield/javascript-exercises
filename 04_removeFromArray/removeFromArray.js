9b12fe3e7e188ecc9e465dd6a19fcafc3e35cf20
Sam Hadfield <sthtech@protonmail.com>
2024-10-30 11:28:24 -0500

    Write function to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, ...theArgs) {
+//   const filtered = arr.filter(function (value) {
+//     return !theArgs.includes(value);
+//   });
+
+//   return filtered;
+// };
+
+const removeFromArray = function (arr, ...theArgs) {
+  const filtered = arr.filter(function (value) {
+    return !theArgs.includes(value);
+  });
+
+  return filtered;
 };
+
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+
+// Pseudocode
+// create new array to store values
+// Loop through original array and rest parameter array (theArgs)
+// if numbers from original array are found within theArgs then 'continue'
+// if numbers from original array are NOT found within theArgs, push them to the new array you have created
+
+// callback function has three parameters: value, index, array
 
 // Do not edit below this line
 module.exports = removeFromArray;
