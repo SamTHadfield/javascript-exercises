b1cabda7d487cffb3711a825258df8bab240fda0
Sam Hadfield <sthtech@protonmail.com>
2024-10-31 09:47:26 -0500

    Write function to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const convertedToCelsius = (fahrenheit - 32) * (5 / 9);
+
+  return Math.round(convertedToCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(-100);
+
+const convertToFahrenheit = function (celsius) {
+  const convertedToFahrenheit = celsius * (9 / 5) + 32;
+
+  return Math.round(convertedToFahrenheit * 10) / 10;
 };
+
+convertToFahrenheit(73.2);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
