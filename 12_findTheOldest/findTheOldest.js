9340ebaf8c20c64fa129fae38f165597ff80bdd6
Sam Hadfield <sthtech@protonmail.com>
2025-01-24 10:32:22 -0600

    Finish function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+// Pseudocode
+// - Loop through array and place Name and Age into new object
+// - Need to subtract the yearofBirth from the yearOfDeath
+/// - before placing in the object
+// - Need to sort finding and return the name with the highest age.
 
+const findTheOldest = function (people) {
+  const newArr = people
+    .map((person) => {
+      if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
+      person.age = person.yearOfDeath - person.yearOfBirth;
+      return person;
+    })
+    .sort((a, b) => b.age - a.age);
+  return newArr[0];
 };
 
 // Do not edit below this line
