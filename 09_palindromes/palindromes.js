244776a404f32e7bbf3481e58819c0cab32b24d4
Sam Hadfield <sthtech@protonmail.com>
2025-01-24 10:31:51 -0600

    Finish function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const splitStr = str.split("");
+  const filtered = splitStr.filter((char) => {
+    if (char !== "!" && char !== " " && char !== "." && char !== ",")
+      return char;
+  });
+  const lowerCasedArr = filtered.map((item) => item.toLowerCase());
+  const lowerCasedStr = lowerCasedArr.join("");
 
+  const reversedArr = lowerCasedArr.reverse();
+  const reversedStr = reversedArr.join("");
+
+  if (lowerCasedStr === reversedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
